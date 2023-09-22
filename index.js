e9573a1f5cf81dd6e01f867be8fc53d51bc867d1
Haris Ahmad <119689990+harisahmad59@users.noreply.github.com>
2023-09-22 20:37:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,11 +10,10 @@ import qr from "qr-image";
 import fs from "fs";
 inquirer
   .prompt([
-    /* Pass your questions in here */
     { message: "type in your URL", name: "URL" },
   ])
   .then((answers) => {
-    // Use user feedback for... whatever!!
+  
     const url = answers.URL;
     var qr_svg = qr.image(url);
     qr_svg.pipe(fs.createWriteStream("qr_img.png"));
